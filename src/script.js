3cc7f8ba1dc0c39b36bb9f8c20a76d353330d956
Leonardo Carvalho <leonardoscc15@gmail.com>
2023-03-01 23:32:55 -0300

    [Easing] move cameraGroup to get easing effect on parallax
    calculate the distance from current position to the destination

--- src/script.js
+++ src/script.js
@@ -155,8 +155,8 @@ const tick = () =>
     const parallaxX = cursor.x 
     const parallaxY = - cursor.y
 
-    cameraGroup.position.x = parallaxX
-    cameraGroup.position.y = parallaxY
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 0.1
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 0.1
 
     
     // Animate meshes

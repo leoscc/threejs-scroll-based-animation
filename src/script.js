1ebaa7a849e5fd25d08001edb3009d9a39ecc3a1
Leonardo Carvalho <leonardoscc15@gmail.com>
2023-03-06 19:58:00 -0300

    [Rotation trigger] rotate mesh when trigger section

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
+import { gsap } from 'gsap'
 
 /**
  * Debug
@@ -167,7 +168,15 @@ document.addEventListener('scroll', () => {
     if(newSection !== currentSection) {
         currentSection = newSection
 
-        console.log(sectionMeshes[currentSection])
+        gsap.to(
+            sectionMeshes[currentSection].rotation,
+            {
+                duration: 1.5,
+                ease: "power2.inOut",
+                x: "+=6",
+                y: "+=3"
+            }
+        )
     }
 })
 
@@ -204,8 +213,8 @@ const tick = () =>
     
     // Animate meshes
     for(const mesh of sectionMeshes) {
-        mesh.rotation.x = elapsedTime * 0.1
-        mesh.rotation.y = elapsedTime * 0.12
+        mesh.rotation.x += deltaTime * 0.1
+        mesh.rotation.y += deltaTime * 0.12
     } 
 
     // Render
